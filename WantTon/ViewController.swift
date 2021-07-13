9ea60fa171c193848f5887d9828f4b30e4dcc5c4
Shreeya Indap <indap.shreeya@icloud.com>
2021-07-13 15:31:50 -0700

    initial set up

--- WantTon/ViewController.swift
+++ WantTon/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let name = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        name.text = "HEY TEAM <3"
+        name.font = UIFont.boldSystemFont(ofSize: 15)
+        name.textColor = UIColor.lightGray
+        name.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(name)
+        
+        setUpConstraints()
+    }
+    
+    func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            name.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            name.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ])
     }
 
 

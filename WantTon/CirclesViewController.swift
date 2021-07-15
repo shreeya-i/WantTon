3041315f7311166849e85a13adf2b43031ceb89b
Shreeya Indap <indap.shreeya@icloud.com>
2021-07-15 16:10:10 -0700

    no colors

--- WantTon/CirclesViewController.swift
+++ WantTon/CirclesViewController.swift
@@ -11,6 +11,8 @@ class CirclesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
 
         // Do any additional setup after loading the view.
     }

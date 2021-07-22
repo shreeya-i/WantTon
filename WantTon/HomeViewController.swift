2e9c12da3bbd147ba099bf463cf844960acf1fc0
Shreeya Indap <indap.shreeya@icloud.com>
2021-07-22 16:45:15 -0700

    hard coded posts

--- WantTon/HomeViewController.swift
+++ WantTon/HomeViewController.swift
@@ -12,20 +12,33 @@ class HomeViewController: UIViewController {
     private var createPostButton = UIButton()
     private var sampleimage = UIImageView()
     let postsTableView = UITableView()
+    let cellReuseIdentifier = "hello"
+    
+    var postData : [Post] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         
+        createDummyData()
         setUpViews()
         setUpConstraints()
 
         // Do any additional setup after loading the view.
     }
     
+    func createDummyData() {
+        let samplepost = Post(image: "carrots.jpeg", caption: "homegrown carrots")
+        let samplepost2 = Post(image: "potatoes.jpeg", caption: "3 potatoes")
+        postData = [samplepost, samplepost2, samplepost, samplepost, samplepost2, samplepost, samplepost2]
+    }
+    
     func setUpViews() {
         postsTableView.translatesAutoresizingMaskIntoConstraints = false
+        postsTableView.register(PostTableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+   //     postsTableView.delegate = self
+        postsTableView.dataSource = self
         view.addSubview(postsTableView)
         
         createPostButton.backgroundColor = UIColor(red: 123/255, green: 190/255, blue: 123/255, alpha: 1.0)
@@ -45,24 +58,24 @@ class HomeViewController: UIViewController {
     
     func setUpConstraints() {
         NSLayoutConstraint.activate([
-            postsTableView.topAnchor.constraint(equalTo: createPostButton.bottomAnchor, constant: 50),
+            postsTableView.topAnchor.constraint(equalTo: createPostButton.bottomAnchor, constant: 30),
             postsTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             postsTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             postsTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
         
         NSLayoutConstraint.activate([
-            createPostButton.topAnchor.constraint(equalTo: sampleimage.bottomAnchor, constant: 50),
+            createPostButton.topAnchor.constraint(equalTo: sampleimage.bottomAnchor, constant: 30),
             createPostButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             createPostButton.widthAnchor.constraint(equalToConstant: 200),
-            createPostButton.heightAnchor.constraint(equalToConstant: 70),
+            createPostButton.heightAnchor.constraint(equalToConstant: 50),
         ])
         
         NSLayoutConstraint.activate ([
             sampleimage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            sampleimage.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -70),
-            sampleimage.heightAnchor.constraint(equalToConstant: 300),
-            sampleimage.widthAnchor.constraint(equalToConstant: 300)
+            sampleimage.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -200),
+            sampleimage.heightAnchor.constraint(equalToConstant: 200),
+            sampleimage.widthAnchor.constraint(equalToConstant: 200)
         ])
     }
     
@@ -89,5 +102,25 @@ extension HomeViewController: UIImagePickerControllerDelegate, UINavigationContr
             return
         }
         sampleimage.image = image
+    }
+}
+
+//extension HomeViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let cell = tableView.cellForRow(at: indexPath) as! PostTableViewCell
+//        let postObject = postData[indexPath.row]
+//    }
+//}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = postsTableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! PostTableViewCell
+        let post = postData[indexPath.row]
+        cell.configure(with: post)
+        return cell
     }
 }
--- WantTon/PostTableViewCell.swift
+++ WantTon/PostTableViewCell.swift
@@ -10,9 +10,11 @@ import UIKit
 class PostTableViewCell: UITableViewCell {
     
     let productImage = UIImageView()
-
+    let caption = UILabel()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle  = .none
         
         setUpViews()
         setUpConstraints()
@@ -20,10 +22,33 @@ class PostTableViewCell: UITableViewCell {
     
     func setUpViews(){
         
+        productImage.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(productImage)
+        
+        caption.font = UIFont.boldSystemFont(ofSize: 17)
+        caption.textColor = UIColor(red: 45/225, green: 128/225, blue: 36/225, alpha: 1.0)
+        caption.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(caption)
+        
     }
     
     func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            productImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 40),
+            productImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -60),
+            productImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            productImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
         
+        NSLayoutConstraint.activate([
+            caption.topAnchor.constraint(equalTo: productImage.bottomAnchor, constant: 10),
+            caption.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8)
+        ])
+    }
+    
+    func configure(with post: Post){
+        productImage.image = UIImage(named: post.image)
+        caption.text = post.caption
     }
     
     required init?(coder: NSCoder) {

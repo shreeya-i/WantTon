00a70f7d08871437f1e0899871224b34f8d80a61
sneha_squasher <sneha.sivakumar@yale.edu>
2021-07-24 14:12:48 -0400

    firebase elements we might need

--- WantTon/AppDelegate.swift
+++ WantTon/AppDelegate.swift
@@ -11,7 +11,8 @@ import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    private let database = Database.database().reference()
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.

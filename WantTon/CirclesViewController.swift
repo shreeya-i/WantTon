0f50c668586cb83b58f3363323437e1dbbe5e9fe
Shreeya Indap <indap.shreeya@icloud.com>
2021-07-27 17:19:53 -0700

    im bored for now

--- WantTon/CirclesViewController.swift
+++ WantTon/CirclesViewController.swift
@@ -8,15 +8,28 @@
 import UIKit
 
 class CirclesViewController: UIViewController {
+    
+    let circlesLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-       // self.navigationController?.isNavigationBarHidden = true
-
-        // Do any additional setup after loading the view.
+        
+        circlesLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        circlesLabel.textColor = UIColor(red: 45/225, green: 128/225, blue: 36/225, alpha: 1.0)
+        circlesLabel.text = "Circles"
+        circlesLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(circlesLabel)
+        
+        setUpConstraints()
     }
     
+    func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            circlesLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 35),
+            circlesLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        ])
+    }
 
 }

f0a1e39d87ad5201257e867f1934344f70e8c9c9
Shimli Satpathy <shimli@bu.edu>
2021-08-04 00:06:27 +0530

    changing background and setting up table view for notifications page

--- WantTon/NotificationsViewController.swift
+++ WantTon/NotificationsViewController.swift
@@ -8,7 +8,22 @@
 import UIKit
 import UserNotifications
 
-class NotificationsViewController: UIViewController {
+class NotificationsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
     
     let notificationsLabel = UILabel()
     let todaylabel = UILabel()
@@ -21,40 +36,30 @@ class NotificationsViewController: UIViewController {
     let notif3 = UILabel()
     let time3 = UILabel()
     let image3 = UIImageView()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Notifications"
         view.backgroundColor = .white
+        
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "background")
+        backgroundImage.contentMode = .scaleAspectFill
+        view.insertSubview(backgroundImage, at: 0)
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
      
         setUpViews()
         setUpConstraints()
+        localNotify()
         
-        // Asking for permission
-        let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
-        }
-        
-        // Creating notification content
-        let content = UNMutableNotificationContent()
-        content.title = "New Notification Alert"
-        content.body = "Open the app WanTon!"
-        
-        // Creating notification trigger
-        let date = Date().addingTimeInterval(5)
-        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour], from: date)
-        
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
-        
-        // Creating request
-        let uuidString = UUID().uuidString
-        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+    }
     
-        // Registering the request
-        center.add(request) { (error) in
-            // check error parameter and handle any errors
-        }
-        
-    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
     
     func setUpViews() {
@@ -71,6 +76,33 @@ class NotificationsViewController: UIViewController {
             notificationsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
         ])
         
+    }
+    
+    func localNotify() {
+        // Asking for permission
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+        }
+        
+        // Creating notification content
+        let content = UNMutableNotificationContent()
+        content.title = "New Notification Alert"
+        content.body = "Open the app WanTon!"
+        
+        // Creating notification trigger
+        let date = Date().addingTimeInterval(5)
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        // Creating request
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+    
+        // Registering the request
+        center.add(request) { (error) in
+            // check error parameter and handle any errors
+        }
     }
     
 }

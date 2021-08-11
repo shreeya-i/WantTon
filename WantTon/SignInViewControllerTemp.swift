7230dd91059d14aaf69b7c420234c8cf351a315c
Shreeya Indap <indap.shreeya@icloud.com>
2021-08-11 16:32:20 -0700

    little fixes

--- WantTon/SignInViewControllerTemp.swift
+++ WantTon/SignInViewControllerTemp.swift
@@ -52,6 +52,7 @@ class SignInViewControllerTemp: UIViewController {
         passwordField.borderStyle = UITextField.BorderStyle.roundedRect
         passwordField.keyboardType = UIKeyboardType.default
         passwordField.placeholder = "Password"
+        passwordField.isSecureTextEntry = true
         passwordField.backgroundColor = UIColor(red: 249/255, green: 249/255, blue: 249/255, alpha: 1.0)
         view.addSubview(passwordField)
         
@@ -176,7 +177,7 @@ class SignInViewControllerTemp: UIViewController {
             }
                           
                 guard error == nil else {
-                strongSelf.showCreateAccount(email: email,password:password)
+             //  strongSelf.showCreateAccount(email: email,password:password)
                 // show account creation
                 return
             }
@@ -196,37 +197,37 @@ class SignInViewControllerTemp: UIViewController {
         
     }
     
-    func showCreateAccount(email: String, password: String){
-         let  alert = UIAlertController(title: "Create Account",
-                                    message: "Would you like to create an account",
-                                    preferredStyle: .alert)
-          alert.addAction(UIAlertAction(title:"Continue",
-                                        style: .default,
-                                        handler: {_ in
-                                            FirebaseAuth.Auth.auth().createUser(withEmail: email, password: email, completion: {[weak self] result, error in
-                                                  guard let strongSelf = self else{
-                                                        return
-                                                    }
-                                                            
-                                                  guard error == nil else {
-                                                      print("Account creation failed")
-                                                  // show account creation
-                                                      return
-                                              }
-                                                  print("you have signed in")
-                                                  strongSelf.titleLabel.isHidden = true
-                                                  strongSelf.emailField.isHidden = true
-                                                  strongSelf.passwordField.isHidden = true
-                                                  strongSelf.button.isHidden = true
-                                              })
-           }))
-          alert.addAction(UIAlertAction(title:"Cancel",
-                                        style: .cancel,
-                                        handler:{_ in
-          }))
-          
-        present(alert, animated:true)
-          }
+//    func showCreateAccount(email: String, password: String){
+//         let  alert = UIAlertController(title: "Create Account",
+//                message: "Would you like to create an account",
+//                preferredStyle: .alert)
+//          alert.addAction(UIAlertAction(title:"Continue",
+//                                        style: .default,
+//                                        handler: {_ in
+//                                            FirebaseAuth.Auth.auth().createUser(withEmail: email, password: email, completion: {[weak self] result, error in
+//                                                  guard let strongSelf = self else{
+//                                                        return
+//                                                    }
+//
+//                                                  guard error == nil else {
+//                                                      print("Account creation failed")
+//                                                  // show account creation
+//                                                      return
+//                                              }
+//                                                  print("you have signed in")
+//                                                  strongSelf.titleLabel.isHidden = true
+//                                                  strongSelf.emailField.isHidden = true
+//                                                  strongSelf.passwordField.isHidden = true
+//                                                  strongSelf.button.isHidden = true
+//                                              })
+//           }))
+//          alert.addAction(UIAlertAction(title:"Cancel",
+//                                        style: .cancel,
+//                                        handler:{_ in
+//          }))
+//
+//        present(alert, animated:true)
+//          }
                                         
 }
             
